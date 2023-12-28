88371138a5f001f5d711c90fcd29bbbd56e86115
Christian Lee <christianlee4037@gmail.com>
2023-12-28 12:16:32 -0800

    Updated time logic to reflect agreed upon logic

--- server/middleware/assignParking.js
+++ server/middleware/assignParking.js
@@ -1,15 +1,20 @@
-const { ParkingSpots, Reservations } = require('../db.js');
+const { ParkingSpots, Reservations } = require("../db.js");
 
 const assignParking = async (req, res, next) => {
-  const { data: ParkingData, error: ParkingError } = await ParkingSpots
-    .select(`id`)
-    .eq('garage_id', req.body.garage_id)
-    .order('id', { ascending: true })
+  const { data: ParkingData, error: ParkingError } = await ParkingSpots.select(
+    `id`
+  )
+    .eq("garage_id", req.body.garage_id)
+    .order("id", { ascending: true });
 
-  const { data: ReservationData, error: ReservationError } = await Reservations
-    .select(`parking_spot_id`)
-    .match({ date: req.body.date, time: req.body.time, garage_id: req.body.garage_id })
-    .order('parking_spot_id', { ascending: true })
+  const { data: ReservationData, error: ReservationError } =
+    await Reservations.select(`parking_spot_id`)
+      .match({
+        date: req.body.date,
+        time: req.body.time,
+        garage_id: req.body.garage_id,
+      })
+      .order("parking_spot_id", { ascending: true });
 
   if (ParkingError || ReservationError) {
     console.log(ParkingError);
@@ -26,12 +31,14 @@ const assignParking = async (req, res, next) => {
       parkingSpot = spot.id;
     }
   }
-
+  if (ReservationData.length === 0) {
+    parkingSpot = ParkingData[0].id;
+  }
   if (!parkingSpot) {
     return res.sendStatus(400);
   }
-  req.body['parking_spot_id'] = parkingSpot;
+  req.body["parking_spot_id"] = parkingSpot;
   next();
-}
+};
 
 module.exports = assignParking;
