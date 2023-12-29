633600a3143c5b8a562c3d3da6ec752e5933a9aa
Christian Lee <christianlee4037@gmail.com>
2023-12-29 15:02:10 -0800

    Thunked axios call in AddressForm

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
 const reservationRouter = require("./routes/reservationsRouter.js");
 const geocodeRouter = require("./routes/geocodeRouter.js");
@@ -11,7 +10,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "../parKing-client")));
 
 // send a query with address key (in the form of a string)
 app.use("/geocode", geocodeRouter);
