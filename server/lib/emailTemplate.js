cdfe98bd79ed178c990b0be5535ff58dd98ea2fa
bzw24 <brucewong24@gmail.com>
2024-01-05 00:13:58 -0800

    updated email template

--- server/lib/emailTemplate.js
+++ server/lib/emailTemplate.js
@@ -14,27 +14,34 @@ const emailTemplate = (data, qr_code) => {
       state: garageState,
       country: garageCountry,
       zip: garageZip,
-    }} = data;
+    },
+  } = data;
 
-  return (`
-    <div style="font-family: Arial, sans-serif; padding: 20px;">
-      <h1 style="color: #333;">Reservation Confirmation</h1>
-      <p>Your Reservation Details:</p>
+  return `
+    <div style="font-family: Arial, sans-serif; padding: 20px; border: 1px solid #ddd; border-radius: 5px; width: fit-content;">
+      <div style="background-color: #e5e7eb; color: black; padding: 10px; text-align: center; border-radius: 5px 5px 0 0; margin-bottom: 10px;">
+      <img src="cid:unique@kreata.ee" alt="parKing" style="max-width: 100px;">
+        <h1 style="margin: 0;">Reservation Confirmation</h1>
+      </div>
+      <p>Dear Customer,</p>
+      <p>We are pleased to confirm your reservation with the following details:</p>
       <ul>
-        <li>Parking Spot ID: ${parking_spot_id}</li>
-        <li>Date: ${date}</li>
-        <li>Time: ${convertTime(time)}</li>
-        <li>Status: ${status}</li>
+        <li><strong>Parking Spot ID:</strong> ${parking_spot_id}</li>
+        <li><strong>Date:</strong> ${date}</li>
+        <li><strong>Time:</strong> ${convertTime(time)}</li>
+        <li><strong>Status:</strong> ${status}</li>
       </ul>
       <p>Garage Details:</p>
       <ul>
-        <li>Name: ${garageName}</li>
-        <li>Address: ${garageAddress}, ${garageCity}, ${garageState}, ${garageCountry}, ${garageZip}</li>
+        <li><strong>Name:</strong> ${garageName}</li>
+        <li><strong>Address:</strong> ${garageAddress}, ${garageCity}, ${garageState}, ${garageCountry}, ${garageZip}</li>
       </ul>
-      <p>QR Code:</p>
-      <img src="${qr_code}" alt="QR Code" style="max-width: 100%;">
+      <p><strong>QR Code:</strong></p>
+      <img src="${qr_code}" alt="QR Code" style="max-width: 100%; margin-bottom: 20px;">
+      <p>We look forward to serving you. If you have any questions or need further assistance, please feel free to contact us.</p>
+      <p>Best Regards,<br>parKing Reservations</p>
     </div>
-  `)
-}
+  `;
+};
 
 module.exports = emailTemplate;
